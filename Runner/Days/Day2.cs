016bde54d87ec4ddfd3535a958fd7b2002a135d0
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2023-12-03 20:38:15 +0100

    Day2 part2 done

--- Runner/Days/Day2.cs
+++ Runner/Days/Day2.cs
@@ -1,51 +1,54 @@
 ﻿namespace Runner.Days;
 
-// Constraints: 12 red, 13 green, 14 blue
-// 2239
 public class Day2 : IExercise<int>
 {
-    private readonly Dictionary<string, int> _maxCountByColor = new()
-    {
-        { "red", 12 },
-        { "green", 13 },
-        { "blue", 14 }
-    };
-    
     public int Test(string[] input)
     {
-        int possibleGameIdsSum = 0;
+        int cubesPower = 0;
+        var countByColor = new Dictionary<string, int>
+        {
+            { Color.Red, 0 },
+            { Color.Green, 0 },
+            { Color.Blue, 0 }
+        };
         for (int i = 0; i < input.Length; i++)
         {
-            string currentGame = input[i];
-            var splitted = currentGame.Split(':');
-            int gameId = int.Parse(splitted[0].Split(' ')[1]);
-            var cubeSets = splitted[1].Split(';');
             
-            bool isGamePossible = true;
-            int j = 0;
-            while (isGamePossible && j < cubeSets.Length)
+            string currentGame = input[i];
+            var cubeSets = currentGame.Split(':')[1].Split(';');
+
+            for (int j = 0; j < cubeSets.Length; j++)
             {
                 var currentSet = cubeSets[j].Split(',', StringSplitOptions.TrimEntries);
                 for (int k = 0; k < currentSet.Length; k++)
                 {
-                    var spl = currentSet[k].Split(' ');
-                    int cubeCount = int.Parse(spl[0]);
-                    string color = spl[1];
-                    if (cubeCount > _maxCountByColor[color])
+                    var cubePair = currentSet[k].Split(' ');
+                    int cubeCount = int.Parse(cubePair[0]);
+                    string color = cubePair[1];
+                    if (cubeCount > countByColor[color])
                     {
-                        isGamePossible = false;
-                        break;
+                        countByColor[color] = cubeCount;
                     }
                 }
-                j++;
             }
 
-            if (isGamePossible)
-            {
-                possibleGameIdsSum += gameId;
-            }
+            cubesPower +=
+                countByColor[Color.Red] *
+                countByColor[Color.Green] *
+                countByColor[Color.Blue];
+            
+            countByColor[Color.Red] = 0;
+            countByColor[Color.Green] = 0;
+            countByColor[Color.Blue] = 0;
         }
 
-        return possibleGameIdsSum;
+        return cubesPower;
     }
+
+    private static class Color
+    {
+        public const string Red = "red";
+        public const string Green = "green";
+        public const string Blue = "blue";
+    } 
 }
