0a2ce405c02d5ad00bfac734214566f1a2902376
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2023-12-09 17:11:33 +0100

    Day4 part2 done

--- Runner/Days/Day4.cs
+++ Runner/Days/Day4.cs
@@ -4,24 +4,38 @@ public class Day4 : IExercise<int>
 {
     public int Test(string[] input)
     {
-        int sum = 0;
+        Dictionary<int, int> matchesByCardId = [];
+        Dictionary<int, int> countByCardId = Enumerable
+            .Range(1, input.Length)
+            .ToDictionary(x => x, _ => 1);
+
         for (int i = 0; i < input.Length; i++)
         {
             var getNumbers = (string input) => input
                 .Trim()
                 .Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-            var numbers = input[i].Split(':')[1].Trim().Split('|');
+            var splitted = input[i].Split(':');
+            string cardId = splitted[0].Split(' ', StringSplitOptions.RemoveEmptyEntries)[1];
+            var numbers = splitted[1].Trim().Split('|');
             var winningNumbers = getNumbers(numbers[0]);
-            var myNumbers = getNumbers(numbers[1]).ToHashSet();
-            int count = winningNumbers.Count(myNumbers.Contains);
-            if (count == 0)
-            {
-                continue;
-            }
-            sum += (int)Math.Pow(2, count - 1);
+            var myNumbers = getNumbers(numbers[1]);
+            int matches = winningNumbers.Count(myNumbers.Contains);
+            int cardNumber = int.Parse(cardId);
+
+            matchesByCardId.Add(cardNumber, matches);
         }
 
-        return sum;
+        foreach (var item in matchesByCardId)
+        {
+            int copies = countByCardId[item.Key];
+            int start = item.Key + 1;
+            for (int j = start; j < start + item.Value; j++)
+            {
+                countByCardId[j] += copies;
+            }
+        }
+
+        return countByCardId.Values.Sum();
     }
 }
