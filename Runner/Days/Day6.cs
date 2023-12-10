d43af12b5a4a00e851049e228ddedda0a86b2ef6
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2023-12-10 10:03:30 +0100

    Day6 part1 done

---
+++ Runner/Days/Day6.cs
@@ -0,0 +1,36 @@
+﻿namespace Runner.Days;
+
+internal class Day6 : IExercise<int>
+{
+    public int Test(string[] input)
+    {
+        var getValues = (string input) => input
+            .Split(':', StringSplitOptions.TrimEntries)[1]
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse);
+
+        var times = getValues(input[0]).ToArray();
+        var distances = getValues(input[1]).ToArray();
+
+        List<int> ways = [];
+        int localCount = 0;
+        for (int i = 0; i < times.Length; i++)
+        {
+            int currentRecord = distances[i];
+            for (int j = 0; j < times[i]; j++)
+            {
+                int currentDistance = (times[i] - j) * j;
+                if (currentDistance > currentRecord)
+                {
+                    localCount++;
+                }
+            }
+
+            ways.Add(localCount);
+            localCount = 0;
+        }
+
+
+        return ways.Aggregate((cur, x) => cur * x);
+    }
+}
--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,9 +1,9 @@
 ﻿using Runner.Days;
 
-var day = new Day5();
+var day = new Day6();
 
 Console.WriteLine("Running app");
-string filePath = $"{Directory.GetCurrentDirectory()}\\Inputs\\{nameof(Day5)}.txt";
+string filePath = $"{Directory.GetCurrentDirectory()}\\Inputs\\{nameof(Day6)}.txt";
 string[] input = File.ReadAllLines(filePath);
 var result = day.Test(input);
 Console.WriteLine(result);
