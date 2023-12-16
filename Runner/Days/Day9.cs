c53111945d805c57a5689400ae09a06913be04f9
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2023-12-16 22:28:03 +0100

    Day9 part1 done

---
+++ Runner/Days/Day9.cs
@@ -0,0 +1,38 @@
+﻿namespace Runner.Days;
+
+internal class Day9 : IExercise<int>
+{
+    public int Test(string[] input)
+    {
+        int sum = 0;
+
+        for (int j = 0; j < input.Length; j++)
+        {
+            int[] history = input[j]
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            List<List<int>> mainInterpolations = [[..history]];
+            List<int> temp = [];
+            while (mainInterpolations[^1].Any(x => x != 0))
+            {
+                for (int i = 1; i < mainInterpolations[^1].Count; i++)
+                {
+                    temp.Add(mainInterpolations[^1][i] - mainInterpolations[^1][i - 1]);
+                }
+                mainInterpolations.Add(new(temp));
+                temp.Clear();
+            }
+
+            // This might not be accurate in every case
+            int final = mainInterpolations
+                .Select(x => x[^1])
+                .Sum();
+
+            sum += final;
+        }
+
+        return sum;
+    }
+}
--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,10 +1,10 @@
 ﻿using Runner.Days;
 
 
-var day = new Day8();
+var day = new Day9();
 
 Console.WriteLine("Running app");
-string filePath = $"{Directory.GetCurrentDirectory()}\\Inputs\\{nameof(Day8)}.txt";
+string filePath = $"{Directory.GetCurrentDirectory()}\\Inputs\\{nameof(Day9)}.txt";
 string[] input = File.ReadAllLines(filePath);
 var result = day.Test(input);
 Console.WriteLine(result);
